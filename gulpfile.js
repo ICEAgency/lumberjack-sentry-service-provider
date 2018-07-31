8d2603e3e91449407ddf0bcf8aa94b6898bfd674
Luke Hopkins <luke@theiceagency.co.uk>
2018-07-31 17:12:02 +0100

    Set up local testing gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var phpcs = require('gulp-phpcs');
+var phpunit = require('gulp-phpunit');
+var notify = require('gulp-notify');
+
+gulp.task('phplint', function () {
+    return gulp.src(['src/**/*.php', 'tests/**/*.php'])
+        .pipe(phpcs({
+            bin: 'vendor/bin/phpcs',
+            standard: 'PSR2',
+            warningSeverity: 0
+        }))
+        .pipe(phpcs.reporter('log'));
+});
+
+gulp.task('phpunit', function () {
+    var options = {
+        debug: false
+    };
+
+    return gulp.src('phpunit.xml')
+        .pipe(phpunit('./vendor/bin/phpunit', options));
+});
+
+gulp.task('test', ['phpunit', 'phplint']);
